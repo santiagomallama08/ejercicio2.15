2c24f4e334076821ab9d283927d01106c8a09a57
SANTIAGO <SANTIAGO@LAPTOP-REHH1RB2>
2022-11-13 12:48:11 -0600

    Agregar archivos de proyecto.

---
+++ ejercicio2.15/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace cuadrado
+{
+    public class cuadrado
+    {
+        public static void Main()
+        {
+            int numero = 821;
+            double cuadrado;
+            cuadrado = Math.Pow(numero, 2);
+
+            Console.WriteLine($"el resultado del cuadrado 821 es {cuadrado}");
+        }
+    }
+}
